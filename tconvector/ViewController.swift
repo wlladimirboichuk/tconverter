5652079045b7491f6f155e0b5d5bb04010e9679a
wlladimirboichuk <wlladimirboichuk@gmail.com>
2020-05-02 19:51:42 +0300

    Add Constrains

--- tconvector/ViewController.swift
+++ tconvector/ViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var canliusLabel: UILabel!
+    @IBOutlet weak var fahrenheitLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    
+    @IBAction func sliderChenged(_ sender: UISlider) {
     }
-
-
+    
 }
 
