e9c2038800c7ebc4139c53fb2891920bfba1f924
wlladimirboichuk <wlladimirboichuk@gmail.com>
2020-05-02 20:28:29 +0300

    Add code

--- tconvector/ViewController.swift
+++ tconvector/ViewController.swift
@@ -12,9 +12,23 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var canliusLabel: UILabel!
     @IBOutlet weak var fahrenheitLabel: UILabel!
-    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var slider: UISlider! {
+
+    didSet {
+        slider.maximumValue = 100
+        slider.minimumValue = 0
+        slider.value = 0
+    }
+    }
+    
+    
+    // Code for label.
     
     @IBAction func sliderChenged(_ sender: UISlider) {
+        let temperatureCelsius = Int (round(sender.value))
+        canliusLabel.text = "\(temperatureCelsius)ºC"
+        let fahrenheitTemperature = Int(round((sender.value * 9 / 5) + 32))
+        fahrenheitLabel.text = "\(fahrenheitTemperature)ºF"
     }
     
 }
